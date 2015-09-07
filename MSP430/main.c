2cef043ec16eec561f4abcaf6ffcdf551941df69
Mario Osborn <mosborn1987@knights.ucf.edu>
2015-09-06 22:00:59 -0400

    This is an attempt to create a LPM3 timer.

--- MSP430/main.c
+++ MSP430/main.c
@@ -11,18 +11,89 @@
 #include <Mx2125.h>
 #include <my_Mx2125.h>
 
+void time_delay( unsigned int time_delay );
 
 void main(void)
 {
 	// Initialize
 	WDTCTL = WDTPW + WDTHOLD;     	// Stop WDT
+	pinMODE(P1_0, OUTPUT);
 
-//	infinit_samples();
-	LED_Sample();
+	while(1)
+	{
+		time_delay(65535);
+		Toggle_GPIO(P1_0);
+
+
+	}
+
+	// infinit_samples();
+//	LED_Sample();
 
 
 }
 
+//////////////////////////////////////////////////////////////////
+// Seconds delay
+void time_delay( unsigned int time_delay )
+{
+//	_EINT();
+//	_BIS_SR(GIE);         			// Enable Interrupt
+
+	//////////////////////////////////////////////////////////////
+	// Enable CCR0 interrupt
+	CCTL0 = CCIE;
+
+	TAR = 0;
+
+	//////////////////////////////////////////////////////////////
+	// Set the micro second time delay
+	CCR0 = time_delay;
+
+	//////////////////////////////////////////////////////////////
+	// Calibrates the DCO Clock to 1MHZ. The DCO clock is the
+	// clock signal source of the SMCLK
+	BCSCTL1 = CAL_BC1_1MHZ; //DIVA_3+;
+
+	//////////////////////////////////////////////////////////////
+	// TaSSEL_1 - selects the ACLK as the clock source
+	// MC_3     - selects the mode. UP/DOWN Mode in this case
+	// ID_3     - Input Signal divider. /8
+	TACTL = TASSEL_1 + MC_3 + ID_3;
+
+//	_EINT();
+	//////////////////////////////////////////////////////////////
+	// CPUOFF - Accomplishes the same results as LPM0
+	_BIS_SR(GIE);//LPM3_bits + GIE);
+
+//	_DINT();
+	return;
+
+}
+
+
+//////////////////////////////////////////////////////////////////
+// Timer A0 interrupt service routine
+#pragma vector=TIMER0_A0_VECTOR //TIMERA0_VECTOR
+__interrupt void Timer_A (void)
+{
+	/////////////////////////////////////////////////////////////
+	// Clear interrupt flag
+	TACTL &= ~TAIFG;
+
+	//////////////////////////////////////////////////////////////
+	// Disable CCR0 interrupt
+	CCTL0 &= ~CCIE;//CCTL0;
+
+	//////////////////////////////////////////////////////////////
+	// Exit LPM0
+//	_BIC_SR_IRQ(LPM3_EXIT);
+
+	//////////////////////////////////////////////////////////////
+	// Disable global interrupts
+	_DINT();
+
+}
 
 
 
